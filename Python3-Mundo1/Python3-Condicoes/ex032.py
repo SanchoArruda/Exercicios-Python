f8270247de37caa86dd4d74608091a1acd64b1a5
Sancho Augusto <gucairesarruda@gmail.com>
2025-03-16 16:06:16 -0300

    organizando

--- Python3-Mundo1/Python3-Condicoes/ex032.py
+++ Python3-Mundo1/Python3-Condicoes/ex032.py
@@ -1,7 +1,10 @@
-ano = int(input('Escreva o ano que quer: '))
+from datetime import date
 
+ano = int(input('Escreva o ano que quer(digite 0 se for calcular o atual): '))
+
+if ano == 0:
+    ano = date.today().year
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
     print("O ano {} eh bissexto".format(ano))
 else:
     print('O ano não eh bissexto')
-    
--- Python3-Mundo1/Python3-Condicoes/ex035.py
+++ Python3-Mundo1/Python3-Condicoes/ex035.py
@@ -3,6 +3,6 @@ r2 = int(input('Escreva o comprimeiro do segundo lado: '))
 r3 = int(input('Escreva o comprimeiro do terceiro lado: '))
 
 if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
-    print('TRUE - os lados formam um triangulo')
+    print('\033[32mTRUE\033[m - os lados formam um triangulo')
 else:
     print('false - nao forma um triangulo')
