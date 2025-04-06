88db3581465af7ba5515bdc8bf4cd70c68d3e00b
Sancho Augusto <gucairesarruda@gmail.com>
2025-04-06 15:54:38 -0300

    add

--- Python3-Mundo2/Python3 - while/ex057.py
+++ Python3-Mundo2/Python3 - while/ex057.py
@@ -1,6 +1,9 @@
 
-s1 = 'M','F'
-while s1 == 'M' and s1 == 'F':
-    s1 = str(input('Digite o seu sexo:(F/M)')).upper()
-    print('Digitou sem F/M, digite nesse formato')
+s1 = ''
+while s1 != 'M' and s1 != 'F': 
+    s1 = str(input('Digite o seu sexo:(F/M): ')).upper()
+    if s1 != 'M' and s1 != 'F':
+        print('Digitou sem F/M, digite nesse formato')
+    else: 
+        print('Parabens voce é inteligente o sufiente! kk')
 print('FIM')
