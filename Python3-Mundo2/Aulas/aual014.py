2ac4d874771d4077cef2d2dea18d5afea094ca04
Sancho Augusto <gucairesarruda@gmail.com>
2025-04-06 11:09:43 -0300

    while

---
+++ Python3-Mundo2/Aulas/aual014.py
@@ -0,0 +1,12 @@
+
+c = 1
+while c <= 10:
+    print(c)
+    c += 1
+print('FIM')
+
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('S/N')).upper()
+print('FIM')
---
+++ Python3-Mundo2/Python3 - while/ex057.py
@@ -0,0 +1,6 @@
+
+s1 = 'M','F'
+while s1 == 'M' and s1 == 'F':
+    s1 = str(input('Digite o seu sexo:(F/M)')).upper()
+    print('Digitou sem F/M, digite nesse formato')
+print('FIM')
