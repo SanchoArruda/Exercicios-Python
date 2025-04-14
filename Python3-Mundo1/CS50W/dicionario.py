a8ace6aaa104ac7730e60501bbcd6b412cf6d3d4
Sancho Augusto <gucairesarruda@gmail.com>
2025-04-14 19:18:39 -0300

    fuctions

---
+++ Python3-Mundo1/CS50W/dicionario.py
@@ -0,0 +1,5 @@
+houses = {"Sancho" : "tiberio", "Luisa"  : "7setembro"}
+
+houses["Adriana"] = "tiberio"
+
+print(houses["Adriana"])
---
+++ Python3-Mundo1/CS50W/quadrado.py
@@ -0,0 +1,4 @@
+from functions import quadrado
+
+for i in range(10):
+    print("O quadrado de {} eh {}".format(i, quadrado(i)))
